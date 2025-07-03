95b2a0e0d549ae98e8f210126a5aa39b0c828817
HCR838 <allprojects026@gmail.com>
2025-07-03 13:31:05 +0530

    Changed path to 'peerjs'

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
+// const express = require('express');
+// const { ExpressPeerServer } = require('peer');
+
+// const app = express();
+// const server = app.listen(process.env.PORT || 3000);
+// const peerServer = ExpressPeerServer(server, {
+//   debug: true,
+//   path: '/'
+// });
+
+// app.use('/peerjs', peerServer);
+
+// console.log("PeerJS server is running...");
+
+
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
 
 const app = express();
 const server = app.listen(process.env.PORT || 3000);
+
 const peerServer = ExpressPeerServer(server, {
   debug: true,
-  path: '/'
+  path: 'peerjs'  // ✅ Fixed this!
 });
 
 app.use('/peerjs', peerServer);
+
+// Optional: nice homepage response
+app.get('/', (req, res) => {
+  res.send('PeerJS server is up and running!');
+});
 
 console.log("PeerJS server is running...");
